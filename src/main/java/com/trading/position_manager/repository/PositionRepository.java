7135d0b85d0a0debfa555f20e3fc1d0eabaa1be9
Sreenand76 <sreenands93@gmail.com>
2026-05-09 14:04:19 +0530

    SCRUM-40: add repositories for position recalculation

---
+++ src/main/java/com/trading/position_manager/repository/PositionRepository.java
@@ -0,0 +1,14 @@
+package com.trading.position_manager.repository;
+
+import com.trading.position_manager.model.Position;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PositionRepository extends JpaRepository<Position, Long> {
+
+    Optional<Position> findByInstrumentId(Long instrumentId);
+
+    List<Position> findAllByInstrumentActiveTrue();
+}
